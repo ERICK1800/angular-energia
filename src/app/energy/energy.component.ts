c4cc72d0d339819157bab21cc4da284c937a4f39
ERICK1800 <ericklimaflorencio@gmail.com>
2023-04-04 10:57:15 -0300

    criando lista

--- src/app/energy/energy.component.ts
+++ src/app/energy/energy.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+interface Item {
+  name: string;
+  time: number;
+}
+
 @Component({
   selector: 'app-energy',
   templateUrl: './energy.component.html',
   styleUrls: ['./energy.component.css']
 })
 export class EnergyComponent {
+  items: Item[] = [];
 
+  addItem() {
+    this.items.push({ name: 'Novo Item', time: 1 });
+  }
+
+  removeItem() {
+    this.items.pop();
+  }
+
+  clearList() {
+    this.items = [];
+  }
+
+  calculate() {
+    const total = this.items.reduce((acc, item) => {
+      return acc + item.time;
+    }, 0);
+    alert(`Total de tempo de uso: ${total} anos`);
+  }
 }
