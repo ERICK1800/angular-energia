2c1349751c4efb6061e59e921aac4fc34f465202
ERICK1800 <ericklimaflorencio@gmail.com>
2023-04-11 16:31:10 -0300

    Calculo do gasto

--- src/app/energy/energy.component.ts
+++ src/app/energy/energy.component.ts
@@ -7,52 +7,34 @@ import { Component } from '@angular/core';
 })
 export class EnergyComponent {
 
-  quantM: number = 0;
-  quantC: number = 0;
-  maquinaT: number = 0;
-  maquinaV: string = "";
+  membroQ: number = 0;
+  comodoQ: number = 0;
+  maquinaLaV: number = 0;
+  maquinaLaT: number = 0;
+  maquinaSeV: number = 0;
+  maquinaSeT: number = 0;
   computQ: number = 0;
   computT: number = 0;
-  tarifa: number = 0;
+  tarifaE: number = 0;
+  tarifaD: number = 0;
   resup: string = "";
 
-  calcular(){}
+  consumoDiarioM: number = 0;
+  consumoDiarioC: number = 0;
+  consumoDiarioL: number = 0;
+  consumoDiarioS: number = 0;
+  consumoDiarioP: number = 0;
+  consumoDiarioTotal: number = 0;
+  custoMensal: string = "";
 
-/*
-  items: any[] = [];
-
-  addItem() {
-    this.items.push({
-      nome: '',
-      quantidade: null,
-      tempo: null
-    });
+  calcular(){
+    this.consumoDiarioM = 100*this.membroQ; // Membro da casa
+    this.consumoDiarioC = 132*this.comodoQ; // Comodo da casa
+    this.consumoDiarioL = (800*this.maquinaLaT)*this.maquinaLaV; // Máquina de lavar
+    this.consumoDiarioS = (300*this.maquinaSeT)*this.maquinaSeV; // Máquina de secar
+    this.consumoDiarioP = (5*this.computT)*this.computQ; // PCs e TVs
+    this.consumoDiarioTotal = ((this.consumoDiarioM)+(this.consumoDiarioC)+(this.consumoDiarioL)+(this.consumoDiarioS)+(this.consumoDiarioP))/1000;
+    this.custoMensal = (((this.consumoDiarioTotal*30)*this.tarifaE)+this.tarifaD).toFixed(2);
+    this.resup = 'Seu gasto de energia por mês é de aproximadamente R$ ' + this.custoMensal;
   }
-
-  removeItem(index: number) {
-    this.items.splice(index, 1);
-  }
-
-  clearList() {
-    this.items = [];
-  }
-
-  calculate() {
-    let totalTempo = 0;
-    for (let item of this.items) {
-      totalTempo += item.quantidade * item.tempo;
-    }
-    this.totalTempo = totalTempo;
-  }
-
-  get totalTempo(): number {
-    return this._totalTempo;
-  }
-
-  set totalTempo(value: number) {
-    this._totalTempo = value;
-  }
-
-  private _totalTempo: number = 0;
-*/
 }
